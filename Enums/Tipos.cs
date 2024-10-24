67867062281881e7da0fe227d2e6af9eaab0ce4c
JoaoFilipe-RSI <115697090+JoaoFilipe-RSI@users.noreply.github.com>
2024-10-24 12:05:10 +0100

    Classes
    Classes principais e Enums de Tipos

---
+++ Enums/Tipos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjCondominios.Enums
+{
+    public enum TipoMorador
+    {
+        Proprietario = 1,
+        Inquilino = 2,
+        Visitante = 3
+    }
+
+    public enum TipoCondominio
+    {
+        Residencial = 1,
+        Comercial = 2,
+        Fechado = 3
+    }
+}
