a5ef906b11834f4e33595690818381d622ae55f8
JoaoFilipe-RSI <115697090+JoaoFilipe-RSI@users.noreply.github.com>
2024-10-24 17:36:33 +0100

    Classes dos serviços + Form menu principal

---
+++ Models/Models/Condominio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjCondominios.Enums;
+
+namespace ProjCondominios.Models
+{
+    public class Condominio
+    {
+        #region Propriedades Privadas
+        private List<Morador> moradores;
+        private List<Reserva> reservas;
+        #endregion
+
+        #region Propriedades Públicas
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public TipoCondominio Tipo { get; set; }
+        #endregion
+
+        #region Construtores
+        public Condominio(int id, string nome, string endereco, TipoCondominio tipo)
+        {
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+            Tipo = tipo;
+            moradores = new List<Morador>();
+            reservas = new List<Reserva>();
+        }
+        #endregion
+
+        #region Métodos
+        #endregion
+    }
+}
---
+++ Models/Models/Pagamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjCondominios.Models
+{
+    public class Pagamento
+    {
+        #region Propriedades Públicas
+        public int Id { get; private set; }
+        public decimal Valor { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; private set; }
+        #endregion
+
+        #region Construtores
+        public Pagamento(int id, decimal valor, string descricao)
+        {
+            Id = id;
+            Valor = valor;
+            Descricao = descricao;
+            Data = DateTime.Now;
+        }
+        #endregion
+
+        #region Métodos
+        #endregion
+    }
+}
