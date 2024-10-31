969e1a5b76cf1ee69b4d1967bb440ffafce5fb66
JoaoFilipe-RSI <jff.tratave@gmail.com>
2024-10-31 10:42:17 +0000

    my first commit

---
+++ ProjCondominios/MenuPrincipal.cs
@@ -0,0 +1,35 @@
+namespace ProjCondominios
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void adicionarNovoCondominioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listarTodosOsMoradoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void adicionarNovoCondominioToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fecharToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ ProjCondominios/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using ProjCondominios.Models;
+using ProjCondominios.Enums;
+using ProjCondominios.Services;
+
+
+
+namespace ProjCondominios
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MenuPrincipal());
+        }
+    }
+}
